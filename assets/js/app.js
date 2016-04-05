9e025c913808a47114cf85fe425602ea08377b58
hashqu <hashqu@gmail.com>
2016-04-05 21:38:43 +0200

    f

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-var map, featureList, boroughSearch = [], theaterSearch = [], museumSearch = [];
+var map, featureList, plagesSearch = [], theaterSearch = [], museumSearch = [];
 
 $(window).resize(function() {
   sizeLayerControl();
@@ -24,7 +24,7 @@ $("#about-btn").click(function() {
 });
 
 $("#full-extent-btn").click(function() {
-  map.fitBounds(boroughs.getBounds());
+  map.fitBounds(plages.getBounds());
   $(".navbar-collapse.in").collapse("hide");
   return false;
 });
@@ -138,7 +138,7 @@ var highlightStyle = {
   radius: 10
 };
 
-var boroughs = L.geoJson(null, {
+var plages = L.geoJson(null, {
   style: function (feature) {
     return {
       color: "black",
@@ -148,7 +148,7 @@ var boroughs = L.geoJson(null, {
     };
   },
   onEachFeature: function (feature, layer) {
-    boroughSearch.push({
+    plagesSearch.push({
       name: layer.feature.properties.BoroName,
       source: "Boroughs",
       id: L.stamp(layer),
@@ -156,123 +156,123 @@ var boroughs = L.geoJson(null, {
     });
   }
 });
-$.getJSON("data/boroughs.geojson", function (data) {
-  boroughs.addData(data);
+$.getJSON("data/plages.geojson", function (data) {
+  plages.addData(data);
 });
 
-var subwayLines = L.geoJson(null, {
-  style: function (feature) {
-    if (feature.properties.route_id === "1" || feature.properties.route_id === "2" || feature.properties.route_id === "3") {
-      return {
-        color: "#ff3135",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "4" || feature.properties.route_id === "5" || feature.properties.route_id === "6") {
-      return {
-        color: "#009b2e",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "7") {
-      return {
-        color: "#ce06cb",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "A" || feature.properties.route_id === "C" || feature.properties.route_id === "E" || feature.properties.route_id === "SI" || feature.properties.route_id === "H") {
-      return {
-        color: "#fd9a00",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "Air") {
-      return {
-        color: "#ffff00",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "B" || feature.properties.route_id === "D" || feature.properties.route_id === "F" || feature.properties.route_id === "M") {
-      return {
-        color: "#ffff00",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "G") {
-      return {
-        color: "#9ace00",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "FS" || feature.properties.route_id === "GS") {
-      return {
-        color: "#6e6e6e",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "J" || feature.properties.route_id === "Z") {
-      return {
-        color: "#976900",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "L") {
-      return {
-        color: "#969696",
-        weight: 3,
-        opacity: 1
-      };
-    }
-    if (feature.properties.route_id === "N" || feature.properties.route_id === "Q" || feature.properties.route_id === "R") {
-      return {
-        color: "#ffff00",
-        weight: 3,
-        opacity: 1
-      };
-    }
-  },
-  onEachFeature: function (feature, layer) {
-    if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Division</th><td>" + feature.properties.Division + "</td></tr>" + "<tr><th>Line</th><td>" + feature.properties.Line + "</td></tr>" + "<table>";
-      layer.on({
-        click: function (e) {
-          $("#feature-title").html(feature.properties.Line);
-          $("#feature-info").html(content);
-          $("#featureModal").modal("show");
-
-        }
-      });
-    }
-    layer.on({
-      mouseover: function (e) {
-        var layer = e.target;
-        layer.setStyle({
-          weight: 3,
-          color: "#00FFFF",
-          opacity: 1
-        });
-        if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-        }
-      },
-      mouseout: function (e) {
-        subwayLines.resetStyle(e.target);
-      }
-    });
-  }
-});
-$.getJSON("data/subways.geojson", function (data) {
-  subwayLines.addData(data);
-});
+// var subwayLines = L.geoJson(null, {
+//   style: function (feature) {
+//     if (feature.properties.route_id === "1" || feature.properties.route_id === "2" || feature.properties.route_id === "3") {
+//       return {
+//         color: "#ff3135",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "4" || feature.properties.route_id === "5" || feature.properties.route_id === "6") {
+//       return {
+//         color: "#009b2e",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "7") {
+//       return {
+//         color: "#ce06cb",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "A" || feature.properties.route_id === "C" || feature.properties.route_id === "E" || feature.properties.route_id === "SI" || feature.properties.route_id === "H") {
+//       return {
+//         color: "#fd9a00",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "Air") {
+//       return {
+//         color: "#ffff00",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "B" || feature.properties.route_id === "D" || feature.properties.route_id === "F" || feature.properties.route_id === "M") {
+//       return {
+//         color: "#ffff00",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "G") {
+//       return {
+//         color: "#9ace00",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "FS" || feature.properties.route_id === "GS") {
+//       return {
+//         color: "#6e6e6e",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "J" || feature.properties.route_id === "Z") {
+//       return {
+//         color: "#976900",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "L") {
+//       return {
+//         color: "#969696",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//     if (feature.properties.route_id === "N" || feature.properties.route_id === "Q" || feature.properties.route_id === "R") {
+//       return {
+//         color: "#ffff00",
+//         weight: 3,
+//         opacity: 1
+//       };
+//     }
+//   },
+//   onEachFeature: function (feature, layer) {
+//     if (feature.properties) {
+//       var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Division</th><td>" + feature.properties.Division + "</td></tr>" + "<tr><th>Line</th><td>" + feature.properties.Line + "</td></tr>" + "<table>";
+//       layer.on({
+//         click: function (e) {
+//           $("#feature-title").html(feature.properties.Line);
+//           $("#feature-info").html(content);
+//           $("#featureModal").modal("show");
+// 
+//         }
+//       });
+//     }
+//     layer.on({
+//       mouseover: function (e) {
+//         var layer = e.target;
+//         layer.setStyle({
+//           weight: 3,
+//           color: "#00FFFF",
+//           opacity: 1
+//         });
+//         if (!L.Browser.ie && !L.Browser.opera) {
+//           layer.bringToFront();
+//         }
+//       },
+//       mouseout: function (e) {
+//         subwayLines.resetStyle(e.target);
+//       }
+//     });
+//   }
+// });
+// $.getJSON("data/subways.geojson", function (data) {
+//   subwayLines.addData(data);
+// });
 
 /* Single marker cluster layer to hold all clusters */
 var markerClusters = new L.MarkerClusterGroup({
@@ -368,9 +368,9 @@ $.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
 });
 
 map = L.map("map", {
-  zoom: 10,
-  center: [40.702222, -73.979378],
-  layers: [mapquestOSM, boroughs, markerClusters, highlight],
+  zoom: 15,
+  center: [40.702222, -3.979378],
+  layers: [mapquestOSM, plages, markerClusters, highlight],
   zoomControl: false,
   attributionControl: false
 });
@@ -484,7 +484,7 @@ var groupedOverlays = {
     "<img src='assets/img/museum.png' width='24' height='28'>&nbsp;Museums": museumLayer
   },
   "Reference": {
-    "Boroughs": boroughs,
+    "Plages": plages,
     "Subway Lines": subwayLines
   }
 };
@@ -513,18 +513,18 @@ $("#featureModal").on("hidden.bs.modal", function (e) {
 $(document).one("ajaxStop", function () {
   $("#loading").hide();
   sizeLayerControl();
-  /* Fit map to boroughs bounds */
-  map.fitBounds(boroughs.getBounds());
+  /* Fit map to plages bounds */
+  map.fitBounds(plages.getBounds());
   featureList = new List("features", {valueNames: ["feature-name"]});
   featureList.sort("feature-name", {order:"asc"});
 
-  var boroughsBH = new Bloodhound({
-    name: "Boroughs",
+  var plagesBH = new Bloodhound({
+    name: "Plages",
     datumTokenizer: function (d) {
       return Bloodhound.tokenizers.whitespace(d.name);
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: boroughSearch,
+    local: plagesSearch,
     limit: 10
   });
 
@@ -578,7 +578,7 @@ $(document).one("ajaxStop", function () {
     },
     limit: 10
   });
-  boroughsBH.initialize();
+  plagesBH.initialize();
   theatersBH.initialize();
   museumsBH.initialize();
   geonamesBH.initialize();
@@ -589,11 +589,11 @@ $(document).one("ajaxStop", function () {
     highlight: true,
     hint: false
   }, {
-    name: "Boroughs",
+    name: "plages",
     displayKey: "name",
-    source: boroughsBH.ttAdapter(),
+    source: plagesBH.ttAdapter(),
     templates: {
-      header: "<h4 class='typeahead-header'>Boroughs</h4>"
+      header: "<h4 class='typeahead-header'>plages</h4>"
     }
   }, {
     name: "Theaters",
@@ -619,7 +619,7 @@ $(document).one("ajaxStop", function () {
       header: "<h4 class='typeahead-header'><img src='assets/img/globe.png' width='25' height='25'>&nbsp;GeoNames</h4>"
     }
   }).on("typeahead:selected", function (obj, datum) {
-    if (datum.source === "Boroughs") {
+    if (datum.source === "plages") {
       map.fitBounds(datum.bounds);
     }
     if (datum.source === "Theaters") {
